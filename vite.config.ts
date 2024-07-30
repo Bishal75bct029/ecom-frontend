0cd0a79a106f4de937fd1fa46d5ef1d4e74e8258
Bishal Lamichhane <bishallc174@gmail.com>
2024-07-30 14:02:39 +0545

    [fix] vite path alias

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,14 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/'),
-      '@assets': `${path.resolve(__dirname, './src/assets/')}`,
+      assets: path.resolve(__dirname, './src/assets/'),
+      layouts: path.resolve(__dirname, './src/layouts/'),
+      components: path.resolve(__dirname, './src/components/'),
+      pages: path.resolve(__dirname, './src/pages/'),
+      store: path.resolve(__dirname, './src/store/'),
+      hooks: path.resolve(__dirname, './src/hooks/'),
+      utils: path.resolve(__dirname, './src/utils/'),
+      routes: path.resolve(__dirname, './src/routes/'),
     },
   },
   server: {
